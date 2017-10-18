4781484a47e6882f920051f4124f670e1ef6cec1
M. Eric Irrgang <ericirrgang@gmail.com>
2017-10-18 17:35:00 -0400

    Clarify test_harmonic

--- plugin/harmonicpotential/tests/test_harmonic.cpp
+++ plugin/harmonicpotential/tests/test_harmonic.cpp
@@ -26,22 +26,38 @@ TEST(HarmonicPotentialPlugin, ForceCalc)
     const vec3<real> e2{real(0), real(1), real(0)};
     const vec3<real> e3{real(0), real(0), real(1)};
 
+    // store temporary values long enough for inspection
+    vec3<real> force{};
+
     plugin::Harmonic puller;
 
     // When input vectors are equal, output vector is meaningless and magnitude is set to zero.
     ASSERT_EQ(real(0.0), norm(puller.calculateForce(e1, e1)));
 
     // Default equilibrium distance is 1.0, so force should be zero when norm(r12) == 1.0.
-    ASSERT_EQ(zerovec, puller.calculateForce(zerovec, e1));
-    ASSERT_EQ(zerovec, puller.calculateForce(e1, zerovec));
-    ASSERT_EQ(zerovec, puller.calculateForce(e1, 2*e1));
+    force = puller.calculateForce(zerovec, e1);
+    ASSERT_EQ(zerovec, force) << " where force is (" << force.x << ", " << force.y << ", " << force.z << ")\n";
+
+    force = puller.calculateForce(e1, zerovec);
+    ASSERT_EQ(zerovec, force) << " where force is (" << force.x << ", " << force.y << ", " << force.z << ")\n";
+
+    force = puller.calculateForce(e1, 2*e1);
+    ASSERT_EQ(zerovec, force) << " where force is (" << force.x << ", " << force.y << ", " << force.z << ")\n";
 
     // -kx should give vector (1, 0, 0) when vector r1 == r2 - (2, 0, 0)
-    ASSERT_EQ(real(1), puller.calculateForce(-2*e1, zerovec).x);
-    ASSERT_EQ(e1, puller.calculateForce(-2*e1, zerovec));
+    force = puller.calculateForce(-2*e1, zerovec);
+    ASSERT_EQ(real(1), force.x);
+    force = puller.calculateForce(-2*e1, zerovec);
+    ASSERT_EQ(e1, force) << " where force is (" << force.x << ", " << force.y << ", " << force.z << ")\n";
 
     // -kx should give vector (-2, 0, 0) when vector r1 == r2 + (2, 0, 0)
-    ASSERT_EQ(-2*e1, puller.calculateForce(2*e1, -e1));
+    force = puller.calculateForce(2*e1, -e1);
+    ASSERT_EQ(-2*e1, force) << " where force is (" << force.x << ", " << force.y << ", " << force.z << ")\n";
+}
+
+TEST(HarmonicPotentialPlugin, Bind)
+{
+
 }
 
 } // end anonymous namespace
