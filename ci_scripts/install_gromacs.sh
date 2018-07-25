3f6bb7cf8ec87d60d2ac5033ee3857cc996e49a5
M. Eric Irrgang <ericirrgang@gmail.com>
2018-07-25 12:17:30 +0300

    Expand continuous integration testing.
    0.0.6 has been tagged in all branches. Also, we will build MPI and test
    multiple compilers.

---
+++ ci_scripts/install_gromacs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ev
+
+export GMX_DOUBLE=OFF
+
+if [ ${CI_MPI} -eq 0 ] ; then
+    export GMX_MPI=OFF
+    export GMX_THREAD_MPI=ON
+else
+    export GMX_MPI=ON
+    export GMX_THREAD_MPI=OFF
+fi
+
+rm -rf build
+mkdir build
+pushd build
+    cmake -DGMX_BUILD_HELP=OFF \
+         -DCMAKE_CXX_COMPILER=$CXX \
+         -DCMAKE_C_COMPILER=$CC \
+         -DGMX_DOUBLE=$GMX_DOUBLE \
+         -DGMX_MPI=$GMX_MPI \
+         -DGMX_THREAD_MPI=$GMX_THREAD_MPI \
+         -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs \
+         ..
+    make -j2 check
+    make -j2 install
+popd
+ccache -s
---
+++ ci_scripts/pygmx_0_0_6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+pushd $HOME
+ git clone --depth=1 --no-single-branch https://github.com/kassonlab/gmxapi.git
+ pushd gmxapi
+  git checkout v0.0.6
+  rm -rf build
+  mkdir -p build
+  pushd build
+   cmake .. -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC -DPYTHON_EXECUTABLE=$PYTHON
+   make -j2 install
+   make -j2 docs
+  popd
+ popd
+ mpiexec -n 2 $PYTHON -m mpi4py -m pytest --log-cli-level=DEBUG --pyargs gmx -s --verbose
+ ccache -s
+popd
