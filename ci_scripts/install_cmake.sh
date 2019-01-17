4cfc7f21b042065fc2e40e9e4a8d8763fd98b5ec
M. Eric Irrgang <ericirrgang@gmail.com>
2019-01-17 16:11:51 +0300

    Fix CMake install.
    We didn't actually install it in the earlier commit...

--- ci_scripts/install_cmake.sh
+++ ci_scripts/install_cmake.sh
@@ -5,21 +5,16 @@
 
 set -ev
 
-mkdir -p ${CMAKE_ROOT}
+mkdir -p ${HOME}/install
+rm -rf ${CMAKE_ROOT}
 
-CMAKE_BUILD=${TRAVIS_BUILD_DIR}/cmake-dist
-mkdir -p ${CMAKE_BUILD}
-
-pushd ${CMAKE_BUILD}
 # we use wget to fetch the cmake binaries
-wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3.tar.gz
+wget -c --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.13.3/cmake-3.13.3-Linux-x86_64.tar.gz
 # this is optional, but useful:
 # do a quick checksum to ensure that the archive we downloaded did not get compromised
-echo "665f905036b1f731a2a16f83fb298b1fb9d0f98c382625d023097151ad016b25  cmake-3.13.3.tar.gz" > cmake_sha.txt
+echo "78227de38d574d4d19093399fd4b40a4fb0a76cbfc4249783a969652ce515270  cmake-3.13.3-Linux-x86_64.tar.gz" > cmake_sha.txt
 sha256sum -c cmake_sha.txt
 # extract the binaries; the output here is quite lengthy,
 # so we swallow it to not clutter up the travis console
-tar -xvf cmake-3.13.3.tar.gz > /dev/null
-mv cmake-3.13.3 cmake-install
-# don't forget to switch back to the main build directory once you are done
-popd
+tar -xvf cmake-3.13.3-Linux-x86_64.tar.gz > /dev/null
+mv cmake-3.13.3-Linux-x86_64 ${CMAKE_ROOT}
