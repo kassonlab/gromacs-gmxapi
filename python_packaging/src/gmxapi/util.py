fa69a9f089ac861e888e675ae99343c6b2b4a0b5
M. Eric Irrgang <ericirrgang@gmail.com>
2019-03-29 17:23:35 +0300

    Support basic command line wrapper in gmxapi
    Introduces gmxapi.commandline module and updates gmxapi.operation
    to support gmxapi.commandline.cli() operation.
    
    TODO: rebase and update when change 9367 is merged
    Change 9367 is a subset of this change that can make this change more
    readable.
    
    Ref: #2894
    
    Change-Id: I9f6c113f21d3a1ce97184a1e48b8bd4856c5c924

--- python_packaging/src/gmxapi/util.py
+++ python_packaging/src/gmxapi/util.py
@@ -34,14 +34,9 @@
 """Utility functions supporting the Gromacs Python interface.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+import os
 
 __all__ = ['which']
-
-import os
 
 from gmxapi import exceptions
 
